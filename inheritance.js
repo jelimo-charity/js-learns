3e242823f5202a57b91dc3956b9f54c138d77f64
jelimo-charity <charityjelimo893@gmail.com>
2022-10-14 22:20:18 -0700

    changes made

---
+++ inheritance.js
@@ -0,0 +1,14 @@
+/*  An example of inheritance in real world;
+There is a base class of "Animal".
+
+There is another class, a sub-class inheriting from "Animal", and the name of this class is "Bird".
+
+Next, there is another class, inheriting from "Bird", and this class is "Eagle".
+
+
+
+
+
+
+
+*/
