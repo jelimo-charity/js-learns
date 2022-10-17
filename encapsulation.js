31a4901687feff677a5a9cbe9ae2cfe77fdf236c
jelimo-charity <charityjelimo893@gmail.com>
2022-10-17 14:21:53 +0300

    changes made

---
+++ encapsulation.js
@@ -0,0 +1,22 @@
+/*
+encapsulation has to do with making a code implementation "hidden" 
+from other users, in the sense that they don't have to know how
+ my code works in order to "consume" the code.
+
+
+
+*/
+//example:
+
+"abc".toUpperCase();
+
+/*
+I don't really need to worry or even waste time thinking about how 
+the toUpperCase() method works. All I want is to use it, since I know 
+it's available to me. Even if the underlying syntax - that is, the 
+implementation of the toUpperCase() method changes - as long as it
+ doesn't break my code, I don't have to worry about what it does in the
+  background, or even how it does it.
+  */
+
+  
---
+++ polymorphism.js
@@ -0,0 +1,22 @@
+/*
+Polymorphism is a word derived from the Greek language meaning "multiple forms".
+ An alternative translation might be: "something that can take on many shapes".
+
+
+So, to understand what polymorphism is about, let's consider some real-life 
+objects.
+
+    >>A door has a bell. It could be said that the bell is a property of the door 
+object. This bell can be rung. When would someone ring a bell on the door? 
+Obviously, to get someone to show up at the door.
+
+    >>Now consider a bell on a bicycle. A bicycle has a bell. It could be said that
+ the bell is a property of the bicycle object. This bell could also be rung. 
+ However, the reason, the intention, and the result of somebody ringing 
+ the bell on a bicycle is not the same as ringing the bell on a door.
+
+
+
+
+*/
+
